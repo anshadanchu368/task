c966f7de39ba6f2dc43679c69c92aca3952bb7a6
anshad <anshadanchu368@example.com>
2022-10-19 12:20:20 +0400

    second commit

--- app.js
+++ app.js
@@ -362,7 +362,13 @@ app.get("/my_uploaded_file", (req, res) => {
   });
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
 
